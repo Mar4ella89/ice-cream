462fc3daddb11ef2c9417365b29c277aa5d48b72
Marat Baikin <3371335@gmail.com>
2022-09-07 04:06:42 +0300

    +

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -1,14 +1,27 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-about-open]"),
-        closeModalBtn: document.querySelector("[data-modal-about-close]"),
-        modal: document.querySelector("[data-modal-about]"),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-about-open]'),
+    closeModalBtn: document.querySelector('[data-modal-about-close]'),
+    modal: document.querySelector('[data-modal-about]'),
+  };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
+
+$('body').on('click', '.js-trailer', function () {
+  $('#trailer-box').dialog({
+    modal: false,
+    show: 'fade',
+    hide: 'fade',
+    width: 800,
+    close: function (event) {
+      $('.modal__video').trigger('pause');
+    },
+  });
+  return false;
+});
